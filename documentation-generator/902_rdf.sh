07ef5a0d4bf2f1138b1405a36535a4246d6ff6fd
Harshvardhan Pandit <me@harshp.com>
2022-05-17 20:32:29 +0100

    merge dpv v0.6.1

---
+++ documentation-generator/902_rdf.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#author: Harshvardhan J. Pandit
+
+#execute DPV RDF generation scripts
+
+CHECK_ERROR() {
+	retval=$?
+	if [ $retval -ne 0 ]; then
+	    echo "Error"
+		exit 1;
+	fi
+}
+
+# Step2: generate RDF
+./002_parse_csv_to_rdf.py
+CHECK_ERROR
+./002_parse_csv_to_rdf_skos.py
+CHECK_ERROR
+./002_parse_csv_to_rdf_owl.py
+CHECK_ERROR
