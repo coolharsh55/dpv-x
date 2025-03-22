6a5da6ca2bc7a4992a6c07bd5fac1d9ac4d095cc
Harshvardhan Pandit <me@harshp.com>
2025-03-22 18:54:13 +0000

    dpvcg issues update

--- dpvcg-issues/fetch.sh
+++ dpvcg-issues/fetch.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#!/bin/bash
-
 # Ensure at least one argument is provided
 if [ "$1" == "-D" ] || [ "$1" == "--download" ]; then
   gh issue list --repo w3c/dpv --state open --limit 200 --json number,title,author,labels > issues.json
